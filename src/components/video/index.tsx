c4d553cc15127da175cf7179e124028b8ad14a3c
Jared Lutz <jared.lutz@matchback.io>
2023-05-25 11:41:10 -0700

    added muted

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -54,6 +54,7 @@ const VideoComponent: FC<IvideosProps> = ({ video, mutate }): JSX.Element => {
                         autoplay
                         chromeless
                         loop
+                        muted
                     />
                     <div onClick={onVideoPress} className={styles.video__press}></div>
 
