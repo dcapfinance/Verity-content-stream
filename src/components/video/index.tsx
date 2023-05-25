e3d3b49adf51a229c6128ae915beb40775636ab2
Jared Lutz <jared.lutz@matchback.io>
2023-05-25 14:39:30 -0700

    ff

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -1,71 +1,78 @@
-import Video from '@api.video/nodejs-client/lib/model/Video'
-import React, { FC, useRef, useState } from 'react'
-import Footer from '../footer'
-import Sidebar from '../sidebar'
-import styles from './videos.module.css'
-import ApiVideoPlayer from '@api.video/react-player'
+import Video from '@api.video/nodejs-client/lib/model/Video';
+import React, { FC, useRef, useState } from 'react';
+import Footer from '../footer';
+import Sidebar from '../sidebar';
+import styles from './videos.module.css';
+import ApiVideoPlayer, { ControlName } from '@api.video/react-player';
 
 export interface IvideosProps {
-    video: Video
-    mutate: () => void
+  video: Video;
+  mutate: () => void;
 }
 
 const VideoComponent: FC<IvideosProps> = ({ video, mutate }): JSX.Element => {
-    const [playing, setPlaying] = useState<boolean>(true)
-    const [showControls, setShowControls] = useState<boolean>(true)
+  const [playing, setPlaying] = useState<boolean>(true);
+  const [showControls, setShowControls] = useState<boolean>(true);
 
-    const { videoId } = video
+  const { videoId } = video;
 
-    const videoRef = useRef<ApiVideoPlayer>(null)
+  const videoRef = useRef<ApiVideoPlayer>(null);
 
-    const togglePlayback = () => {
-        if (playing) {
-            videoRef.current?.pause()
-        } else {
-            videoRef.current?.play()
-        }
-        setPlaying(!playing)
+  const togglePlayback = () => {
+    if (playing) {
+      videoRef.current?.pause();
+    } else {
+      videoRef.current?.play();
     }
+    setPlaying(!playing);
+  };
 
-    const handleVideoPress = () => {
-        setShowControls(!showControls)
-        if (showControls && !playing) {
-            videoRef.current?.play()
-            setPlaying(true)
-        }
+  const handleVideoPress = () => {
+    setShowControls(!showControls);
+    if (showControls && !playing) {
+      videoRef.current?.play();
+      setPlaying(true);
     }
+  };
 
-    const height = window.screen.availHeight - 50
+  const height = window.screen.availHeight - 50;
 
-    return (
-        <>
-            {video && (
-                <div
-                    className={styles.video}
-                    id={videoId}
-                    onClick={handleVideoPress}
-                >
-                    <ApiVideoPlayer
-                        video={{ id: videoId }}
-                        videoStyleObjectFit={'cover'}
-                        ref={videoRef}
-                        style={{
-                            width: screen.width,
-                            height: height,
-                            scrollSnapAlign: 'start',
-                            border: 0,
-                        }}
-                        autoplay
-                        loop
-                        controls={showControls}
-                        onClick={togglePlayback}
-                    />
-                    <Footer video={video} />
-                    <Sidebar video={video} mutate={mutate} />
-                </div>
-            )}
-        </>
-    )
-}
+  // Array of available control button names
+  const controlButtons: ControlName[] = [
+    'play-large',
+    'play',
+    'progress',
+    'current-time',
+    'mute',
+    'volume',
+    'fullscreen',
+  ];
 
-export default VideoComponent
+  return (
+    <>
+      {video && (
+        <div className={styles.video} id={videoId} onClick={handleVideoPress}>
+          <ApiVideoPlayer
+            video={{ id: videoId }}
+            videoStyleObjectFit={'cover'}
+            ref={videoRef}
+            style={{
+              width: screen.width,
+              height: height,
+              scrollSnapAlign: 'start',
+              border: 0,
+            }}
+            autoplay
+            loop
+            controls={showControls ? controlButtons : []} // Pass the array of control buttons only when showControls is true
+            onClick={togglePlayback}
+          />
+          <Footer video={video} />
+          <Sidebar video={video} mutate={mutate} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default VideoComponent;
